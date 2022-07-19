07626a324c2769d205cca9a4de41a52660a69420
230806bing <2284393118@qq.com>
2022-07-19 15:53:11 +0800

    消息通知demo

---
+++ app/src/main/java/com/example/demo/pushdemo/PushActivity.java
@@ -0,0 +1,16 @@
+package com.example.demo.pushdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.demo.R;
+
+public class PushActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push);
+    }
+}
